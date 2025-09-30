b49a4106feafcc802151de5c5ca41a2b849c97e6
Maissen <maissen.developer500@gmail.com>
2025-09-30 14:33:41 +0100

    Update

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -19,11 +19,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import java.text.NumberFormat
 
 class MainActivity : ComponentActivity() {
@@ -31,21 +43,18 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContent {
-            TipTimeTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                ) {
-                    TipTimeLayout()
-                }
-            }
+            TipTimeTheme()
         }
     }
 }
 
 @Composable
 fun EditNumberField(modifier: Modifier = Modifier) {
+
+    val amountInput = "0"
+
     TextField(
-        value = "some text here",
+        value = amountInput,
         onValueChange = {},
         modifier = modifier
     )
@@ -55,17 +64,22 @@ fun EditNumberField(modifier: Modifier = Modifier) {
 fun TipTimeTheme() {
     Column(
         modifier = Modifier
-            .statusBarPadding()
+            .fillMaxSize()
+            .statusBarsPadding()
             .padding(horizontal = 40.dp)
             .verticalScroll(rememberScrollState())
             .safeDrawingPadding(),
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalAlignment = Arrangement.center
+        verticalArrangement = Arrangement.Center
     ) {
         Text(
             text = "Calculate tip",
         )
-        EditNumberField(modifier = Modifier.padding(bottom = 32.dp).fillMaxWidth())
+        EditNumberField(
+            modifier = Modifier
+                .padding(bottom = 32.dp)
+                .fillMaxWidth()
+        )
         Text(
             text = "Tip Amount: 0.0$",
         )
@@ -83,7 +97,5 @@ private fun calculateTip(amount: Double, tipPercent: Double = 15.0, roundUp: Boo
 @Preview(showBackground = true)
 @Composable
 fun TipTimeLayoutPreview() {
-    TipTimeTheme {
-        TipTimeLayout()
-    }
+    TipTimeTheme()
 }
